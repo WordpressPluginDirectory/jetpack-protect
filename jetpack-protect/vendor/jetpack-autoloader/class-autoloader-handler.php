04499db9f193839a580296590305867cf38def4d
Justin Gardner <justin@rhynorater.com>
2024-09-06 06:02:57 -0500

    3.0.1

--- jetpack-protect/vendor/jetpack-autoloader/class-autoloader-handler.php
+++ jetpack-protect/vendor/jetpack-autoloader/class-autoloader-handler.php
@@ -5,7 +5,7 @@
  * @package automattic/jetpack-autoloader
  */
 
-namespace Automattic\Jetpack\Autoloader\jpc4802e05bbcf59fd3b6350e8d3e5482c_protectⓥ3_0_0\al3_0_9;
+namespace Automattic\Jetpack\Autoloader\jpc4802e05bbcf59fd3b6350e8d3e5482c_protectⓥ3_0_1\al3_0_10;
 
  // phpcs:ignore
 
